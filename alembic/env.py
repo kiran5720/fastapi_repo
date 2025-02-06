49a7fc04b1d42622ec41e300d9f733164d78e2da
kiran5720 <kirangowda5720@gmail.com>
2025-02-06 17:10:07 +0530

    Updated alembic.ini 2nd time and env.py for Render deployment

--- alembic/env.py
+++ alembic/env.py
@@ -5,11 +5,13 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from app.database import Base
+from app.models import Base
+from app.config import settings
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-
+config.set_main_option("sqlalchemy.url",f"postgresql+pyscopg2://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}")
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
@@ -22,10 +24,7 @@ if config.config_file_name is not None:
 target_metadata = Base.metadata
 
 
-database_url = os.getenv("DATABASE_URL")
 
-if database_url:
-    config.set_main_option("sqlalchemy.url", database_url)
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
@@ -69,23 +68,14 @@ def run_migrations_online() -> None:
         poolclass=None,
     )
 
+
     with connectable.connect() as connection:
-        context.configure(connection=connection)
+        context.configure(
+            connection=connection, target_metadata=target_metadata
+        )
+
         with context.begin_transaction():
             context.run_migrations()
-    # connectable = engine_from_config(
-    #     config.get_section(config.config_ini_section, {}),
-    #     prefix="sqlalchemy.",
-    #     poolclass=pool.NullPool,
-    # )
-
-    # with connectable.connect() as connection:
-    #     context.configure(
-    #         connection=connection, target_metadata=target_metadata
-    #     )
-
-    #     with context.begin_transaction():
-    #         context.run_migrations()
 
 
 if context.is_offline_mode():
